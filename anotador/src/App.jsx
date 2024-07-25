1489158d362885620d9fe54715099ced85937af0
EdissonFacundo <aquinoedissonfacundo@gmail.com>
2024-07-25 02:06:08 -0300

    modificacion de diseño

--- anotador/src/App.jsx
+++ anotador/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Carpetas from './componentes/carpetas'; // Componente para mostrar las carpetas
 import CarpetasDetalles from './componentes/carpetasDetalles'; // Importa el componente de detalles de carpeta
 import FormCarpeta from './componentes/formCarpeta';
-
 function App() {
   return (
     <Router>
